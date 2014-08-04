1373731217c547ab510d0a2cbf5ae194f1cc78d9
Kevin Bell <kevin.a.bell@gmail.com>
2014-08-03 20:03:23 -0700

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+TAG=$1
+KUBE_CMD=${KUBERNETES_ROOT:-~/kubernetes}/cluster/kubecfg.sh
+
+# Create credential files
+envsubst < .kubernetes_auth.template > ~/.kubernetes_auth
+# envsubst < .dockercfg.template > ~/.dockercfg
+
+# Deploy image to private GCS-backed registry
+docker push $EXTERNAL_REGISTRY_ENDPOINT/hello:$TAG
+
+# Update Kubernetes replicationController
+envsubst < kubernetes/rails-controller.json.template > rails-controller.json
+$KUBE_CMD -c rails-controller.json \
+    update replicationControllers/railscontroller
+
+# Roll over Kubernetes pods
+$KUBE_CMD rollingupdate railscontroller
