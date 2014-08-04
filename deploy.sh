0d9b4f45e10156ae2cae7adc461f426e2ca575ae
Kevin Bell <kevin.a.bell@gmail.com>
2014-08-03 23:01:55 -0700

    Remove deploy script parameter

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # Exit on any error
 set -e
 
-SHA=$1
 KUBE_CMD=${KUBERNETES_ROOT:-~/kubernetes}/cluster/kubecfg.sh
 
 # Create credential files
@@ -11,10 +10,10 @@ envsubst < .kubernetes_auth.template > ~/.kubernetes_auth
 # envsubst < .dockercfg.template > ~/.dockercfg
 
 # Deploy image to private GCS-backed registry
-docker push $EXTERNAL_REGISTRY_ENDPOINT/hello:$SHA
+docker push $EXTERNAL_REGISTRY_ENDPOINT/hello:$CIRCLE_SHA1
 
 # Update Kubernetes replicationController
-TAG=$SHA envsubst < kubernetes/rails-controller.json.template > rails-controller.json
+envsubst < kubernetes/rails-controller.json.template > rails-controller.json
 $KUBE_CMD -c rails-controller.json \
     update replicationControllers/railscontroller
 
