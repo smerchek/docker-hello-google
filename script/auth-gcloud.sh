bd111ca0c7a834deb0e4c1d064e38b08b885d678
Scott Smerchek <scott.smerchek@gmail.com>
2015-07-24 14:39:53 -0500

    Couple customizations

--- script/auth-gcloud.sh
+++ script/auth-gcloud.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo $GCLOUD_KEY | base64 --decode > gcloud.p12
-gcloud auth activate-service-account 484500905059-bb55pm9ophqkbhkd1vrront20ati766f@developer.gserviceaccount.com --key-file gcloud.p12
+gcloud auth activate-service-account $GCLOUD_EMAIL --key-file gcloud.p12
 ssh-keygen -f ~/.ssh/google_compute_engine -N ""
